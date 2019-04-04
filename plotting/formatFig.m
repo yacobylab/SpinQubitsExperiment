090d170f82f5638041df8e5442996a28f2927b7b
spharvey3 <spharvey3@gmail.com>
2019-04-04 19:31:14 -0400

    Improved clarity for formatFig

--- plotting/formatFig.m
+++ plotting/formatFig.m
@@ -1,13 +1,14 @@
 function formatFig(n,opts,nrow,ncol)
-% Tries to make things look nice without taking up too much space. 
-% function formatFig(n,opts,nrow,ncol)
-% n a list of figure numbers to format.
-% opts: 
 % For all, rotate Y axes. Set axes and legend font sizes. 
-% qpc2, qpc are line scans. Set onscreen position differently, diff font size. 
-% chrg, sens: 
-% exch
-% full 
+% Quickly format figures to look nice without taking up too much space
+% function formatFig(n,opts,nrow,ncol)
+% n: A list of figure numbers to format.
+% Sets font size, YTick rotation, position (i.e. size). 
+% opts: 
+% qpc2, qpc: for line scans. Set onscreen position differently, diff font size. 
+% chrg, sens: color plots
+% exch: 
+% full: Compactify everything by moving labels closer to axis. 
 % otherwise: 
 % No option: set a line width. 
 
@@ -17,52 +18,37 @@ for i = 1:length(n)
     axesInds = isgraphics(f.Children,'axes');
     legInds = isgraphics(f.Children,'legend');    
     if isopt(opts, 'qpc2')
-        [f.Children(axesInds).FontSize]=deal(9);
-        if any(legInds)            
-            [f.Children(legInds).FontSize]=deal(6);
-        end
-        [f.Children(axesInds).YTickLabelRotation]=deal(-30);
+        fontSize = 9; 
+        tickRotation = -30; 
+        legFontSize = 6; 
         f.Position = [746 9 932 977];
-        %[f.Children(axesInds).FontSize]=deal(7);        
     elseif isopt(opts,'qpc') 
-        [f.Children(axesInds).FontSize]=deal(7);
-        if any(legInds)            
-            [f.Children(legInds).FontSize]=deal(5);
-        end
-        [f.Children(axesInds).YTickLabelRotation]=deal(-30);
+        fontSize = 7; 
+        tickRotation = -30; 
+        legFontSize = 5; 
         f.Position = [500 150 800 600];
     elseif isopt(opts,'exch')
-        [f.Children(axesInds).FontSize]=deal(9);
-        if any(legInds)
-            [f.Children(legInds).FontSize]=deal(6);
-        end
-        [f.Children(axesInds).YTickLabelRotation]=deal(-30);
+        fontSize = 9; 
+        tickRotation = -30; 
+        legFontSize = 6; 
         f.Position = [500 150 800 600];
     elseif isopt(opts,'chrg')
-        [f.Children(axesInds).FontSize]=deal(8.5);
-        if any(legInds)            
-            [f.Children(legInds).FontSize]=deal(7);
-        end
-        [f.Children(axesInds).YTickLabelRotation]=deal(-30);
+        fontSize = 8.5; 
+        tickRotation = -30; 
+        legFontSize = 7; 
         f.Position = [100 150 800 630];
     elseif isopt(opts,'sens')
-        [f.Children(axesInds).FontSize]=deal(8.5);
-        if any(legInds)            
-            [f.Children(legInds).FontSize]=deal(7);
-        end
-        [f.Children(axesInds).YTickLabelRotation]=deal(-30);
-        f.Position = [900 150 800 630];
-    else
-        ax = gca;
-        h = ax.Children;
-        if ~isempty(h)
-            [h.LineWidth] = deal(2.5);
-        end
-        ax.Box = 'off';
-        ax.FontSize = 24;
-        ax.FontName = 'Arial';
+        fontSize = 8.5; 
+        tickRotation = -30; 
+        legFontSize = 7;                         
+        f.Position = [900 150 800 630];    
     end
-    if isopt(opts,'full')
+    [f.Children(axesInds).FontSize]=deal(fontSize);
+    if any(legInds)            
+            [f.Children(legInds).FontSize]=deal(legFontSize);
+    end
+    [f.Children(axesInds).YTickLabelRotation]=deal(tickRotation);
+    if isopt(opts,'full') % This moves the X and Y labels closer to the axis. 
         axesNum = find(axesInds); 
         xLabOff = [32,16,10]; yLabOff = [23 11 7]; 
         for j = 1:length(axesNum)
