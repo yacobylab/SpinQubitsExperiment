f2e51e120bdf6ed8f9f07caf1d9e2956637c610f
spharvey3 <spharvey3@gmail.com>
2019-04-05 13:29:51 -0400

    Clean up fioscill a bit.

--- fitting/fioscill.m
+++ fitting/fioscill.m
@@ -1,40 +1,32 @@
 function fp = fioscill(x, y, ctrl)
+% Initial guesses for parameters to fit oscillating data
 % function fp = fioscill(x, y, ctrl)
-% ctrl = 1: offset, cos coef, sin coef, freq, shift (always 0), decay prefac = 1/mean(abs(x));
-%   fp: 
-% ctrl = 2: offset, amplitude, phase, freq, shift, decay prefac = 1/mean(abs(x));
-%   fp: 
+% for both:
+% fp(1): y offset, fp(4): frequency fp(5): shift (always 0), decay
+% prefactor (very basic calc, 1/2 of inverse mean time)
+% ctrl = 1: returns fp(2): sin coef, fp(3): cos coef
+% ctrl = 2: returns fp(2): global coef, fp(3): phase
+% returns pars: offset, amplitude, phase, freq, shift, decay prefac = 1/mean(abs(x));
 
-switch ctrl 
+dx = mean(diff(x));
+nx = length(x);
+window=1;
+% hack to get phase shift, factors determined experimentally
+ft = fft((y-mean(y)).*window) .* exp(1i * x(1) * (0:nx-1) * 2*pi /(dx*nx))/nx;
+ft = ft(1:round(end/2));
+[~, freqInd] = max(abs(ft(2:end))); % Find frequency
+xi = max(1,freqInd-2) : min(length(ft)-1,freqInd+2);
+meanxi = sum(abs(ft(xi+1)).*xi)/sum(abs(ft(xi+1)));
+fp(4) = 2 * pi * meanxi / ((nx+1) * dx);
+f = ft(freqInd+1);
+fp(1) = mean(y);
+fp(5) = 0;
+fp(6) = 1 / (2 * mean(abs(x)));
+switch ctrl
     case 1
-        dx = mean(diff(x));
-        window=1;
-        ft = fft((y-mean(y)).*window) .* exp(1i * x(1) * (0:length(x)-1) * 2*pi /(dx*length(x)))/length(x);
-        % hack to get phase shift, factors determined experimentally
-        ft = ft(1:round(end/2));
-        [~, mi] = max(abs(ft(2:end)));
-        xi=max(1,mi-2):min(length(ft)-1,mi+2);           
-        mia=sum(abs(ft(xi+1)).*xi)/sum(abs(ft(xi+1)));
-        fp(4) = 2* pi* (mia)/((length(x)+1) * dx);        
-        f=ft(mi+1);
         fp(3) = -3*real(f);
-        fp(2) = -3*imag(f);        
-        fp(1) = mean(y);
-        fp(5) = 0;
-        fp(6) = .5/mean(abs(x));        
+        fp(2) = -3*imag(f);
     case 2
-        dx = mean(diff(x));
-        window=1;
-        ft = fft((y-mean(y)).*window) .* exp(1i * x(1) * (0:length(x)-1) * 2*pi /(dx*length(x)))/length(x);
-        ft = ft(1:round(end/2));
-        [~, mi] = max(abs(ft(2:end)));
-        xi=max(1,mi-2):min(length(ft)-1,mi+2);           
-        mia=sum(abs(ft(xi+1)).*xi)/sum(abs(ft(xi+1)));
-        fp(4) = 2* pi* (mia)/((length(x)+1) * dx);        
-        f=ft(mi+1);
         fp(3) = angle(f);
-        fp(2) = -3*abs(f);        
-        fp(1) = mean(y);
-        fp(5) = 0;
-        fp(6) = .5/mean(abs(x));  
+        fp(2) = -3*abs(f);
 end
