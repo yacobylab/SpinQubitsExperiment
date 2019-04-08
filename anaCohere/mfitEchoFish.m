00f98bcffbc3bde7b9a5bbb95112062a46e45b79
spharvey3 <spharvey3@gmail.com>
2019-04-07 23:02:09 -0400

    Playing around with how to fix some fitting issues in mfitEchoFish

--- anaCohere/mfitEchoFish.m
+++ anaCohere/mfitEchoFish.m
@@ -7,7 +7,7 @@ function [out,figs]=mfitEchoFish(file,config)
 %   residuals
 %   fishless
 %   noerrorbars
-% opts.taurange ([-inf,inf] default) Which times to use.
+% opts.tvrange ([-inf,inf] default) Which times to use.
 % opts.mfitopts ('plinit plfit plotiterr err' default)
 % opts.frames ; Which reps of data to use (e.g. if gradient runs away,
 % don't use that)
@@ -24,18 +24,27 @@ function [out,figs]=mfitEchoFish(file,config)
 % in out.params, returns with 1st row: J, 2nd: phase, 3rd: fish phase,
 % 4th: y offset 5: t offset (for decay)
 if ~exist('config','var'), config=struct(); end
+if iscell(config), config=struct(config{:}); end
+
 config=def(config,'grps',[1 Inf]);
 config=def(config,'opts','residuals colorplot nocenter plotfit'); % nocenter also allowed
 config=def(config,'taurange',[-Inf Inf]);
 config=def(config,'mfitopts','plinit plfit plotiter err');
 config=def(config,'frames',[]);
 config=def(config,'fignum',1024); fignum = config.fignum;
-config=def(config,'xrng',[-Inf,Inf]); xrng = config.xrng;
+config=def(config,'rng',[-Inf,Inf]); rng = config.rng;
 figs=[];
 nParPerm = 5; nParVar = 5; tCenterVar = 10; phaseVar = 7; freqVar = 6; fishAmp = 4; fishPhase = 8;
 % Kind of annoying but I think all of the vars > 6 are actually smaller by
 % 1 than in fitfn because tau doesn't count.
-if ~exist('file','var') || isempty(file), file = getFiles; end
+if ~exist('file','var') || isempty(file)
+    if ~isopt(config.opts,'nofilt')
+        file=uigetfile('sm*Ramsey*.mat');
+    else
+        file=uigetfile('sm*.mat');
+    end    
+end
+out.file = file; 
 %% Load the data.
 % Get scaled data and pulsegroup info data
 s=procPlsData(file,struct('grps',config.grps,'opts','noplot noppt','frames',config.frames));
@@ -46,7 +55,7 @@ end
 goodTau=find(s.tv > config.taurange(1) & s.tv < config.taurange(2));
 s.tv=s.tv(goodTau); s.grps=s.grps(goodTau);
 
-x=squeeze(nanmean(s.data{1}(:,s.grps,:),1));
+dataAll=squeeze(nanmean(s.data{1}(:,s.grps,:),1));
 if any(any(isnan(s.data{1}))), fprintf('NAN data: variances will be wrong\n'); end
 % denominator here gets variance of mean, only if no NANs in data
 xs=squeeze(nanstd(s.data{1}(:,s.grps,:),1)).^2/(size(s.data{1},1));
@@ -68,7 +77,7 @@ if isopt(config.opts,'colorplot') % Make a 2D color plot of the data and varianc
     [mx,my]=meshgrid(s.xv{config.grps(1)},s.tv);
     %subplot(1,2,1);
     axes(ha(1)); 
-    pcolor(mx,my,x); shading flat; colorbar;
+    pcolor(mx,my,dataAll); shading flat; colorbar;
     xlabel('Time (ns)'); ylabel('\tau (\mu s)');
     title('Singlet Probability');    
     %subplot(1,2,2);
@@ -77,19 +86,18 @@ if isopt(config.opts,'colorplot') % Make a 2D color plot of the data and varianc
     %title('Variance'); 
 end
 %% Stuff the data structure and initial guess
-% Guess T2 = max time, alpha = 1.7, amp = standard deviation of data , T2* = 30 ns
-% FIXME: could make T2* smarter.
 if isopt(config.opts,'fishless')
-    initial=[max(s.tv), 1.3, 2*nanstd(x(1,:)), 0, 10];
+% Guess   T2 = max tau, alpha, amp = std of first row of data, fish amp, T2* = 30 ns
+    initial=[max(s.tv), 1.3, 2*nanstd(dataAll(1,:)), 0, 10];
 else
-    initial=[max(s.tv), 1.3, 5*nanstd(x(1,:)), 2*nanstd(x(1,:)), 10];
+    initial=[max(s.tv), 1.3, 5*nanstd(dataAll(1,:)), 2*nanstd(dataAll(1,:)), 10];
 end
 parInit=[];
 for j=1:length(s.grps) % Collect data to go to mfitwrap
     % Check this: have we not already filtered for groups?
-    goodGrps=find(s.xv{s.grps(j)} > xrng(1) & s.xv{s.grps(j)} < xrng(2));
+    goodGrps=find(s.xv{s.grps(j)} > rng(1) & s.xv{s.grps(j)} < rng(2));
     data(j).x=s.xv{s.grps(j)}(goodGrps);
-    data(j).y=x(j,goodGrps); %#ok<*AGROW>
+    data(j).y=dataAll(j,goodGrps); %#ok<*AGROW>
     data(j).vary=xs(j,goodGrps);
     
     offsetInd = (j-1)*nParVar; currInd = offsetInd + nParPerm;
@@ -98,37 +106,51 @@ for j=1:length(s.grps) % Collect data to go to mfitwrap
     % Write in the fitfn with the current indices. 
     model(j).fn=str2func(sprintf(fitFn, currInd+4,currInd+1,currInd+2,currInd+5,s.tv(j),currInd+1,currInd+3,s.tv(j)));
     parInitOld = parInit;
-    parInit=fioscill(data(j).x,data(j).y,1); % initial guess for freq, phase.
+    parInit=fioscill(data(j).x,data(j).y,2); % initial guess for freq, phase.
     if (nanstd(data(j).y) < 2.8*nanmean(sqrt(data(j).vary))) && j > 1 % propagate forward frequency guess when signal is very small
         %parInit(4)=initial(end-4);
         parInit(2:4) = parInitOld(2:4); 
         %fprintf('Propagating omega forward on group %d due to small signal \n',j);
     end
-    %see above,       freq        phase,              offset,   time offset
-    initial=[initial, parInit(4), atan2(parInit(2),parInit(3)), 0.1, mean(data(j).y), 2];
+    %see above,       freq        phase,           amp offset,   time offset
+    initial=[initial, parInit(4), parInit(3), 0.1, mean(data(j).y), 2];
 end
 
 for i=freqVar:nParVar:length(initial) % Median filter the initial frequency guess.
     initial(i)=median([initial(max(freqVar,i-nParVar)), initial(i), initial(min(end,i+nParVar))]);
 end
-out.s=s; out.mdata=data; out.mmod=model; out.initial=initial; out.p=initial;
+out.s=s; out.mdata=data; out.mmod=model; out.initial=initial; 
+out.p=initial;
 
-% Start with an initial mfitwrap of each data set individually. 
+% Start wby performing mfitwrap of each data set individually. 
+% Allow only echo center and fish phase, phase to be fit. 
+t2s=[];
 for j = 1:length(s.grps)
     mask=initial*0;
     mask(phaseVar+nParVar*(j-1))=1; %phase
-    if ~isopt(config.opts,'fishless')
-        mask(8+nParVar*(j-1))=1; % fish phase
-    end
+    mask(5) = 1; % Try fitting t2*     
     if isopt(config.opts,'nocenter')
         mask(tCenterVar:nParVar:end)=1; % time center
     end
     if isopt(config.opts,'debug') 
-        config.mfitopts = [config.mfitopts,'plinit plfit optimplot'];
+        config.mfitopts = [config.mfitopts,' plinit plfit optimplot'];
+    end    
+    if ~isopt(config.opts,'fishless')
+        mask(8+nParVar*(j-1))=1; % fish phase
+        %m1 = mask; m1(4) = 1; 
+        if j == 1
+            d1.x = data(1).x(1:floor(end/3)); d1.y = data(1).y(1:floor(end/3)); 
+            d1.vary = data(1).vary(1:floor(end/3)); 
+            mod1.fn = @(p,x) p(1) + p(2).*cos(p(4).*x+p(3)).*exp(-((1e3*s.tv(1)/2 + x)/p(5)).^2);
+            beta0 = [0.55, 0.15, out.p(freqVar),3*pi/2,10];
+            pars=mfitwrap(d1,mod1,beta0,config.mfitopts,ones(1,length(beta0))); %out.p is the initial guess.                    
+            
+        end
     end
     out.p=mfitwrap(data(j),model(j),out.p,config.mfitopts,mask); %out.p is the initial guess.
+    t2s = [t2s out.p(5)];
 end
-
+out.p(5) = nanmean(t2s); 
 mask=ones(1,length(initial));
 mask(2)=0; % alpha
 % If all tau >> t2*, won't need to fit initial decay
@@ -144,8 +166,7 @@ end
 [out.p, out.chisq,out.cov]=mfitwrap(data,model,out.p,config.mfitopts,mask);
 mask(2)=1;
 [out.p, out.chisq, out.cov,out.exitflag,out.output]=mfitwrap(data,model,out.p,config.mfitopts,mask);
-
-% Put data in out struct 
+%% Put data in out struct 
 out.t2 = out.p(1); out.alpha=out.p(2); out.amp = out.p(3); out.fishAmp = out.p(4); 
 out.t2s = out.p(5); out.tMax = max(s.tv);
 out.params = reshape(out.p(nParPerm+1:end),nParVar,(length(out.p)-nParPerm)/nParVar);
@@ -154,27 +175,23 @@ out.t2Err = stdData(1); out.alphaErr = stdData(2); out.ampErr = stdData(3);
 out.fishAmpErr=stdData(4); out.t2sErr = stdData(5); 
 stdParams = reshape(stdData(nParPerm+1:end),nParVar,(length(stdData)-nParPerm)/nParVar);
 out.jErr = stdParams(1,:)*1e3/2/pi; 
-out.phaseDataErr = stdParams(2,:); 
-out.ampOffsetErr = stdParams(4,:); 
-out.ampOffset = out.params(4,:); 
-out.tOffset = out.params(5,:); 
+out.phaseDataErr = stdParams(2,:); out.ampOffsetErr = stdParams(4,:); 
+out.ampOffset = out.params(4,:); out.tOffset = out.params(5,:); 
 out.tOffsetErr = stdParams(5,:); 
 freqData = out.params(1,:); 
 out.J = freqData*1e3/2/pi; % In MHz. 
-out.phaseData=out.params(2,:); 
-out.fishPhase = out.params(3,:); 
+out.phaseData=out.params(2,:); out.fishPhase = out.params(3,:); 
 out.phaseDataErr = stdParams(5,:); 
 %% Make plots of fitted data
-
 if isopt(config.opts,'residuals') 
     offset=0.25; % Try making this smart     
     resHt=.18; 
     for j=1:length(out.mdata)
-        x=out.mdata(j).x;
+        dataAll=out.mdata(j).x;
         y=out.mdata(j).y;
         % Plot data                
         if isopt(config.opts,'noerrorbars')
-            plot(ha(axInd),x,y+offset*(j-1),'.');
+            plot(ha(axInd),dataAll,y+offset*(j-1),'.');
         else
             errorbar(ha(axInd),out.mdata(j).x,out.mdata(j).y+offset*(j-1), sqrt(out.mdata(j).vary),'CapSize',2);
         end        
@@ -191,9 +208,9 @@ if isopt(config.opts,'residuals')
         plot(ha(axInd),xVec,yVec+offset*(j-1));        
         hold(ha(axInd+1),'on'); 
         % Plot residual 
-        yVec=out.mmod(j).fn(out.p,x);        
-        plot(ha(axInd+1),x,offset*resHt*(j-1)+x*0,'k'); % zero error line        
-        errorbar(ha(axInd+1),x,y-yVec+offset*resHt*(j-1), sqrt(out.mdata(j).vary),'CapSize',1);        
+        yVec=out.mmod(j).fn(out.p,dataAll);        
+        plot(ha(axInd+1),dataAll,offset*resHt*(j-1)+dataAll*0,'k'); % zero error line        
+        errorbar(ha(axInd+1),dataAll,y-yVec+offset*resHt*(j-1), sqrt(out.mdata(j).vary),'CapSize',1);        
     end    
     out.phaseData = unwrap(out.phaseData,pi/2);     
     out.fishPhase = unwrap(out.phaseData,pi/2);     
